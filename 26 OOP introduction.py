f0d8eb937aafedf23807a7ce553bc14f875b32d2
Bhushan Dnyaneshwar Khedekar <148458560+Bhushankhedekar@users.noreply.github.com>
2026-03-30 17:41:48 +0530

    Add files via upload

---
+++ 26 OOP introduction.py
@@ -0,0 +1,13 @@
+# OOP Introduction
+# 1.sequential programming
+# 2.dunctional programming
+# 3.object oriented programming
+
+class Student:
+    def __init__(self, age, location):
+        self.age = age
+        self.location = location
+        Student1 = Student(26, "abc23")
+        print(Student1.age)
+
+        
